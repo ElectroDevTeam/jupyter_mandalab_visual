892f38d6715336b6f96bddff0d0306649055b782
Amit Klein <amitkle001@gmail.com>
2020-03-30 15:07:29 +0300

    Moved mandalab.py to test

--- tests/test.py
+++ tests/test.py
@@ -1,20 +1,20 @@
-import mandalab_visual
+from mandalab import *
 
 
 def generate_network():
-    m1 = mandalab_visual.mandalab.Machine("m1", on=True)
+    m1 = Machine("m1", on=True)
     m1.add_ports(["eth0", "eth1", "eth2"])
-    m2 = mandalab_visual.mandalab.Machine("m2", on=False)
+    m2 = Machine("m2", on=False)
     m2.add_ports(["eth0", "eth1", "eth33"])
-    m3 = mandalab_visual.mandalab.Machine("m3", on=True, os="windows")
+    m3 = Machine("m3", on=True, os="windows")
     m3.add_port("eth10")
-    net1 = mandalab_visual.mandalab.Network("net1")
+    net1 = Network("net1")
     net1.connect(m1.ports[0])
     net1.connect(m2.ports[0])
     net1.connect(m3.ports[0])
-    m4 = mandalab_visual.mandalab.Machine("m3", on=False, os="windows")
+    m4 = Machine("m3", on=False, os="windows")
     m4.add_port("eth10")
-    net2 = mandalab_visual.mandalab.Network("net2")
+    net2 = Network("net2")
     net2.connect(m4.ports[0])
     net2.connect(m1.ports[1])
     return m1
